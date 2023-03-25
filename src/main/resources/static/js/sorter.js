7bb50dd43137e5272e1c47aeb41213e448a70493
vm <ladvdrikman@gmail.com>
2023-03-25 09:05:06 +0300

    v 2.0.9

--- src/main/resources/static/js/sorter.js
+++ src/main/resources/static/js/sorter.js
@@ -4,13 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
         const order = (target.dataset.order = -(target.dataset.order || -1));
         const index = [...target.parentNode.cells].indexOf(target);
         const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
-        const comparator = (index, order) => (a, b) => order * collator.compare(
-            a.children[index].innerHTML,
-            b.children[index].innerHTML
-        );
+        const tableRowComparator = (index, order) => {
+            return (rowA, rowB) => {
+                const cellA = rowA.children[index].innerHTML;
+                const cellB = rowB.children[index].innerHTML;
+
+// will be false for integer numbers
+                if (isNaN(cellA)) {
+                    return order * collator.compare(cellA, cellB)
+                }
+                return order * (cellA - cellB);
+            };
+        };
 
         for(const tBody of target.closest('table').tBodies)
-            tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+            tBody.append(...[...tBody.rows].sort(tableRowComparator(index, order)));
 
         for(const cell of target.parentNode.cells)
             cell.classList.toggle('sorted', cell === target);
